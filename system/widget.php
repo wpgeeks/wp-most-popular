bcca2eaf72bcc308c020e8c334422902f31c7af6
Matt Geri <mattgeri@gmail.com>
2012-08-05 17:22:15 +0200

    Custom post type support for widget

--- system/widget.php
+++ system/widget.php
@@ -8,15 +8,30 @@ class WMP_Widget extends WP_Widget {
 		$defaults = $this->default_options( $instance );
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label><br />
 			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $defaults['title']; ?>">
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'number' ); ?>">Number of posts to show:</label>
+			<label for="<?php echo $this->get_field_id( 'number' ); ?>">Number of posts to show:</label><br />
 			<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $defaults['number']; ?>" size="3">
 		</p>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'timeline' ); ?>">Timeline:</label>
+			<label for="<?php echo $this->get_field_id( 'post_type' ); ?>">Choose post type:</label><br />
+			<select id="<?php echo $this->get_field_id( 'post_type' ); ?>" name="<?php echo $this->get_field_name( 'post_type' ); ?>">
+				<option value="all">All post types</option>
+				<?php
+				$post_types = get_post_types( array( 'public' => true ), 'names' );
+				foreach ($post_types as $post_type ) {
+					// Exclude attachments
+					if ( $post_type == 'attachment' ) continue;
+					$defaults['post_type'] == $post_type ? $sel = " selected" : $sel = "";
+					echo '<option value="' . $post_type . '"' . $sel . '>' . $post_type . '</option>';
+				}
+				?>
+			</select>
+		</p>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'timeline' ); ?>">Timeline:</label><br />
 			<select id="<?php echo $this->get_field_id( 'timeline' ); ?>" name="<?php echo $this->get_field_name( 'timeline' ); ?>">
 				<option value="all_time"<?php if ( $defaults['timeline'] == 'all_time' ) echo "selected"; ?>>All time</option>
 				<option value="monthly"<?php if ( $defaults['timeline'] == 'monthly' ) echo "selected"; ?>>Past month</option>
@@ -37,11 +52,16 @@ class WMP_Widget extends WP_Widget {
 			$options['number'] = (int) $instance[ 'number' ];
 		else
 			$options['number'] = 5;
-			
+		
+		if ( isset( $instance[ 'post_type' ] ) )
+			$options['post_type'] = esc_attr( $instance[ 'post_type' ] );
+		else
+			$options['post_type'] = 'all';
+
 		if ( isset( $instance[ 'timeline' ] ) )
 			$options['timeline'] = esc_attr( $instance[ 'timeline' ] );
 		else
-			$options['timeline'] = 'all';
+			$options['timeline'] = 'all_time';
 		
 		return $options;
 	}
@@ -56,8 +76,15 @@ class WMP_Widget extends WP_Widget {
 		extract( $args );
 		
 		// Get our posts
-		$defaults	= $this->default_options( $instance );
-		$posts		= wmp_get_popular( array( 'limit' => (int) $defaults[ 'number' ], 'range' => $defaults['timeline'] ) );
+		$defaults			= $this->default_options( $instance );
+		$options['limit']	= (int) $defaults[ 'number' ];
+		$options['range']	= $defaults['timeline'];
+
+		if ( $defaults['post_type'] != 'all' ) {
+			$options['post_type'] = $defaults['post_type'];
+		}
+
+		$posts = wmp_get_popular( $options );
 		
 		// Display the widget
 		echo $before_widget;
