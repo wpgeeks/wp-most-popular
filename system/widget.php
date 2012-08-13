ba32aa7cdabfc42c8d9bf577a257a638c3555332
Robert O'Rourke <robert@o-rourke.org>
2012-08-13 12:55:08 +0100

    Made the widget output easy to mod via code - much preferable to some of the other plugins where users can edit the output template

--- system/widget.php
+++ system/widget.php
@@ -3,7 +3,7 @@ class WMP_Widget extends WP_Widget {
 	public function __construct() {
 		parent::WP_Widget( 'wmp_widget', 'WP Most Popular', array( 'description' => 'Display your most popular blog posts on your sidebar' ) );
 	}
-	
+
 	public function form( $instance ) {
 		$defaults = $this->default_options( $instance );
 		?>
@@ -26,54 +26,58 @@ class WMP_Widget extends WP_Widget {
 		</p>
 		<?php
 	}
-	
+
 	private function default_options( $instance ) {
 		if ( isset( $instance[ 'title' ] ) )
 			$options['title'] = esc_attr( $instance[ 'title' ] );
 		else
 			$options['title'] = 'Popular posts';
-			
+
 		if ( isset( $instance[ 'number' ] ) )
 			$options['number'] = (int) $instance[ 'number' ];
 		else
 			$options['number'] = 5;
-			
+
 		if ( isset( $instance[ 'timeline' ] ) )
 			$options['timeline'] = esc_attr( $instance[ 'timeline' ] );
 		else
 			$options['timeline'] = 'all';
-		
+
 		return $options;
 	}
-	
+
 	public function update( $new, $old ) {
 		$instance = wp_parse_args( $new, $old );
 		return $instance;
 	}
-	
+
 	public function widget( $args, $instance ) {
 		// Find default args
 		extract( $args );
-		
+
 		// Get our posts
 		$defaults	= $this->default_options( $instance );
 		$posts		= wmp_get_popular( array( 'limit' => (int) $defaults[ 'number' ], 'range' => $defaults['timeline'] ) );
-		
+
 		// Display the widget
 		echo $before_widget;
 		if ( $defaults['title'] ) echo $before_title . $defaults['title'] . $after_title;
-		echo '<ul>';
+		echo apply_filters( 'most_popular_list_before', '<ul>' );
 		global $post;
 		foreach ( $posts as $post ):
 			setup_postdata( $post );
-			?>
-			<li><a href="<?php the_permalink() ?>" title="<?php echo esc_attr(get_the_title() ? get_the_title() : get_the_ID()); ?>"><?php if ( get_the_title() ) the_title(); else the_ID(); ?></a></li>
-			<?php
+			do_action( 'most_popular_list_item', $post );
 		endforeach;
-		echo '</ul>';
+		echo apply_filters( 'most_popular_list_after', '</ul>' );
 		echo $after_widget;
-		
+
 		// Reset post data
 		wp_reset_postdata();
+	}
+
+	public function list_item( $post ) {
+		?>
+		<li><a href="<?php the_permalink(); ?>"><?php if ( get_the_title() ) { the_title(); } else { the_ID(); } ?></a></li>
+		<?php
 	}
 }
--- wp-most-popular.php
+++ wp-most-popular.php
@@ -7,18 +7,18 @@
 	Author: Matt Geri
 	Author URI: http://mattgeri.com
 	License: GPL2
-	
+
 	Copyright 2011 Matt Geri (email: mattgeri@gmail.com)
-	
+
 	This program is free software; you can redistribute it and/or modify
-	it under the terms of the GNU General Public License, version 2, as 
+	it under the terms of the GNU General Public License, version 2, as
 	published by the Free Software Foundation.
-	
+
 	This program is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
-	
+
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to the Free Software
 	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
@@ -27,29 +27,29 @@
 if ( phpversion() > 5 ) {
 	// Setup our path
 	define( 'WMP_PATH', dirname(__FILE__) . '/' );
-	
+
 	// Setup activation and deactivation
 	register_activation_hook( WP_PLUGIN_DIR . '/wp-most-popular/wp-most-popular.php', 'WMP_system::install' );
 	register_deactivation_hook( WP_PLUGIN_DIR . '/wp-most-popular/wp-most-popular.php', 'WMP_system::uninstall' );
-	
+
 	// Include our helpers
 	include_once( WMP_PATH . 'system/helpers.php' );
-	
+
 	// Class for installation and uninstallation
 	class WMP_system{
 		public static function actions() {
 			// Check for token
 			if ( ! wp_verify_nonce( $_POST['token'], 'wmp_token' ) ) die();
-			
+
 			include_once( WMP_PATH . 'system/track.php' );
 			$track = new WMP_track( intval( $_POST['id'] ) );
 		}
-		
+
 		public static function install() {
 			include_once( WMP_PATH . 'system/setup.php' );
 			WMP_setup::install();
 		}
-		
+
 		public static function javascript() {
 			global $wp_query;
 			wp_reset_query();
@@ -59,24 +59,25 @@ if ( phpversion() > 5 ) {
 				echo '<!-- WordPress Most Popular --><script type="text/javascript">/* <![CDATA[ */ jQuery.post("' . admin_url('admin-ajax.php') . '", { action: "wmp_update", id: ' . $wp_query->post->ID . ', token: "' . $token . '" }); /* ]]> */</script><!-- /WordPress Most Popular -->';
 			}
 		}
-	
+
 		public static function uninstall() {
 			include_once( WMP_PATH . 'system/setup.php' );
 			WMP_setup::uninstall();
 		}
-		
+
 		public static function widget() {
 			register_widget( 'WMP_Widget' );
 		}
 	}
-	
+
 	// Use ajax for tracking popular posts
 	add_action( 'wp_head', 'WMP_system::javascript' );
 	add_action( 'wp_ajax_wmp_update', 'WMP_system::actions' );
 	// Comment out to stop logging stats for admin and logged in users
 	add_action( 'wp_ajax_nopriv_wmp_update', 'WMP_system::actions' );
-	
+
 	// Widget
 	include_once( WMP_PATH . 'system/widget.php' );
 	add_action( 'widgets_init', 'WMP_system::widget' );
+	add_action( 'most_popular_list_item', array( 'WMP_Widget', 'list_item' ) );
 }
